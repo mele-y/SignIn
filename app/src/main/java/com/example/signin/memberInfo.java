6f49d6edf855439dba8b922c90a1ab72e136c2e4
doublefong <1834249184@qq.com>
2018-12-27 12:59:29 +0800

    create notice/send message

--- app/src/main/java/com/example/signin/memberInfo.java
+++ app/src/main/java/com/example/signin/memberInfo.java
@@ -1,10 +1,13 @@
 package com.example.signin;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.qmuiteam.qmui.widget.grouplist.QMUICommonListItemView;
@@ -17,43 +20,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class memberInfo extends AppCompatActivity {
-     private String name,classId,stu_id,stu_name;
-     private List<signinInfo> data = new ArrayList<>();
-    QMUIGroupListView  day_list;
+    private String name, classId, stu_id, stu_name;
+    private List<signinInfo> data = new ArrayList<>();
+    QMUIGroupListView day_list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         data = singleAttendanceInfo.getAttendances();
-        String title2=singleAttendanceInfo.getRate();//逻辑层获取信息修改title2
+        String title2 = singleAttendanceInfo.getRate();//逻辑层获取信息修改title2
         setContentView(R.layout.activity_member_info);
-        Toolbar stu_toolbar=(Toolbar)findViewById(R.id.member_toolbar);//获取TOOLBAR实例
+        Toolbar stu_toolbar = (Toolbar) findViewById(R.id.member_toolbar);//获取TOOLBAR实例
         setSupportActionBar(stu_toolbar);//把TOOLBAR设为标题栏
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        Intent intent=getIntent();
-        name= (String) intent.getCharSequenceExtra("name");
-        classId= (String) intent.getCharSequenceExtra("classId");//课程信息
-        stu_id=(String)intent.getCharSequenceExtra("stu_id");//学生信息
-        stu_name=(String)intent.getCharSequenceExtra("stu_name");
+        Intent intent = getIntent();
+        name = (String) intent.getCharSequenceExtra("name");
+        classId = (String) intent.getCharSequenceExtra("classId");//课程信息
+        stu_id = (String) intent.getCharSequenceExtra("stu_id");//学生信息
+        stu_name = (String) intent.getCharSequenceExtra("stu_name");
         getSupportActionBar().setTitle(stu_name);
         stu_toolbar.setSubtitle(stu_id);//设置标题与副标题
 
+        FloatingActionButton fab=(FloatingActionButton)findViewById(R.id.send_message);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        day_list=findViewById(R.id.day_list);//获取列表
+                Intent intent=new Intent(memberInfo.this,teasend_message.class);
+                startActivity(intent);
+            }
+        });//点击发送消息,传递学生信息给下一个activity
+
+        day_list = findViewById(R.id.day_list);//获取列表
         day_list.setSeparatorStyle(QMUIGroupListView.SEPARATOR_STYLE_NORMAL);
 
-        QMUIGroupListView.Section section=QMUIGroupListView.newSection(memberInfo.this).setTitle("总出勤率                                                                                                  "+ title2);
-        if(data.size() > 0){
+        QMUIGroupListView.Section section = QMUIGroupListView.newSection(memberInfo.this).setTitle("总出勤率                                                                                                  " + title2);
+        if (data.size() > 0) {
             List<QMUICommonListItemView> lst = new ArrayList<>();
-            for(int i=0;i<data.size();++i){
+            for (int i = 0; i < data.size(); ++i) {
                 QMUICommonListItemView msg = day_list.createItemView(data.get(i).getTime());
                 msg.setDetailText(data.get(i).getAttendance());
                 lst.add(msg);
-                section.addItemView(lst.get(i),null);
+                section.addItemView(lst.get(i), null);
             }
         }
         section.addTo(day_list);//将section加入列表
 
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId()==android.R.id.home)
@@ -62,5 +76,6 @@ public class memberInfo extends AppCompatActivity {
             return  true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }//左上角返回按钮事件
+
 }
--- app/src/main/java/com/example/signin/teasend_message.java
+++ app/src/main/java/com/example/signin/teasend_message.java
@@ -1,8 +1,8 @@
 package com.example.signin;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -11,13 +11,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
-public class createNotice extends AppCompatActivity {
+
+public class teasend_message extends AppCompatActivity {
     private EditText notice_title,notice_context;//编辑框 手机号和密码
     private String title,context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_notice);
+        setContentView(R.layout.activity_create_notice);//和发公告用得同一个layout
         Toolbar toolbar=(Toolbar)findViewById(R.id.create_notice_toolbar);//获取TOOLBAR实例
         setSupportActionBar(toolbar);//把TOOLBAR设为标题栏
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -26,11 +27,11 @@ public class createNotice extends AppCompatActivity {
         QMUIRoundButton btn=(QMUIRoundButton)findViewById(R.id.release);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {//点击发布,逻辑层交接存储数据
+            public void onClick(View v) {//点击发布,逻辑层交接存储数据,内容存储到学生信息,title,context
                 context=notice_context.getText().toString().trim();
                 title=notice_title.getText().toString().trim();
-                Toast.makeText(createNotice.this ,"发布成功",Toast.LENGTH_SHORT).show();
-               finish();//返回界面
+                Toast.makeText(teasend_message.this ,"发送成功",Toast.LENGTH_SHORT).show();
+                finish();//返回界面
             }
         });
     }
@@ -45,3 +46,4 @@ public class createNotice extends AppCompatActivity {
     }
 
 }
+
