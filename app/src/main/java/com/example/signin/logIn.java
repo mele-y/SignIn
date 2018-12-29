5748e3e17b48f11d4ef0886f29e904205e9cb3c8
doublefong <1834249184@qq.com>
2018-12-29 20:58:08 +0800

    .

--- app/src/main/java/com/example/signin/logIn.java
+++ app/src/main/java/com/example/signin/logIn.java
@@ -26,8 +26,8 @@ public class logIn extends AppCompatActivity {
         et_user_phone.setText(userInfo.getPhonenum());
         et_psw=findViewById(R.id.password);
         et_psw.setText(userInfo.getPassword());
-       Button btn_login=findViewById(R.id.btn_login);
-       Button btn_register=findViewById(R.id.btn_register);
+        QMUIRoundButton btn_login=findViewById(R.id.btn_login);
+        QMUIRoundButton btn_register=findViewById(R.id.btn_register);
 
         btn_register.setOnClickListener(new View.OnClickListener()//点击注册按钮,跳转注册界面
         {
